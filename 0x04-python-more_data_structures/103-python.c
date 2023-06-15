38b29439aaa659821c092304aa6b355ddabc23c9
Muliro1 <mulirokhaemba@gmail.com>
2023-06-15 04:07:35 +0300

    last

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -9,7 +9,7 @@
  */
 void print_python_bytes(PyObject *p)
 {
-	char *string;
+	char *str;
 	long int size, i, limit;
 
 	printf("[.] bytes object info\n");
@@ -20,10 +20,10 @@ void print_python_bytes(PyObject *p)
 	}
 
 	size = ((PyVarObject *)(p))->ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
+	str = ((PyBytesObject *)p)->ob_sval;
 
 	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
+	printf("  trying string: %s\n", str);
 
 	if (size >= 10)
 		limit = 10;
@@ -33,10 +33,10 @@ void print_python_bytes(PyObject *p)
 	printf("  first %ld bytes:", limit);
 
 	for (i = 0; i < limit; i++)
-		if (string[i] >= 0)
-			printf(" %02x", string[i]);
+		if (str[i] >= 0)
+			printf(" %02x", str[i]);
 		else
-			printf(" %02x", 256 + string[i]);
+			printf(" %02x", 256 + str[i]);
 
 	printf("\n");
 }
