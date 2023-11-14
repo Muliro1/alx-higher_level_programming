ea276e0bf6f485ea5bc70902c41fb4d5d12cf5a1
Muliro <mulirokhaemba@gmail.com>
2023-11-14 21:10:04 +0300

    almost

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,7 +6,7 @@ import('fs');
  * The first argument is the file path of the first source file.
  * The second argument is the file path of the second source file.
  * The third argument is the file path of the destination.
- 
+
 const fs = import('fs');
 const fileA = process.argv[2];
 const fileB = process.argv[3];
@@ -17,8 +17,6 @@ const dataB = fs.readFileSync(fileB, { encoding: 'utf8' });
 fs.writeFileSync(fileC, dataA + dataB, { encoding: 'utf8' });
 */
 
-
-
 const fs = require('fs');
 
 const concatFiles = (sourceFilePath1, sourceFilePath2, destinationFilePath) => {
@@ -27,7 +25,7 @@ const concatFiles = (sourceFilePath1, sourceFilePath2, destinationFilePath) => {
     const fileContent2 = fs.readFileSync(sourceFilePath2, 'utf-8');
     const concatenatedContent = fileContent1 + fileContent2;
     fs.writeFileSync(destinationFilePath, concatenatedContent);
-    //console.log('Files concatenated successfully!');
+    // console.log('Files concatenated successfully!');
   } catch (error) {
     console.error('Error concatenating files:', error);
   }
