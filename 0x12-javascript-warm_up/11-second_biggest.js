d3389cefca54b8094e9d65664e682173e7d84d88
Muliro <mulirokhaemba@gmail.com>
2023-11-15 00:04:38 +0300

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 // searches the second biggest integer in the list of arguments.
 
 if (process.argv.length <= 3 || process.argv.length === 1) {
-  console.log(0);
+  console.log('0');
 } else {
   const list = process.argv.sort();
   console.log(list.reverse()[1]);
