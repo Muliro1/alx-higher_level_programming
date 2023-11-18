a8835fd50ab8434ca9dcdd4dd70da469889ae61d
Muliro <mulirokhaemba@gmail.com>
2023-11-19 00:53:21 +0300

    meta

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, text
+from sqlalchemy import Column, Integer, String, text, MetaData
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 """
     Module that performs creates a States class based off of Base.
 """
-
-Base = declarative_base()
+meta_data = MetaData()
+Base = declarative_base(metadata = meta_data)
 
 
 class State(Base):
@@ -17,4 +17,4 @@ class State(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", backref="State", cascade="all, delete")
+    cities = relationship("City", backref="state", cascade="all, delete")
