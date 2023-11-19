812143ed168e265e337940b666a66db16d65030f
Muliro <mulirokhaemba@gmail.com>
2023-11-19 11:09:08 +0300

    backref

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -17,4 +17,8 @@ class State(Base):
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", backref="states", cascade="all, delete")
+    cities = relationship(
+        "City",
+        cascade="all, delete-orphan",
+        backref="state", cascade="all",
+        single_parent=True)
