3c79affdf22f8b4887bd14fe5c67e63f5ad542b2
Muliro <mulirokhaemba@gmail.com>
2023-11-30 18:27:20 +0300

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that finds a peak in a list of integers"""
+"""Module that finds a peak in a list of integer"""
 
 
 def find_peak(list_of_integers):
@@ -28,19 +28,19 @@ def find_peak(list_of_integers):
     Returns:
         int: peak(s)
     """
-    list_ = list_of_integers
+    my_list = list_of_integers
     # if there is no list of integers return None
-    if list_ == []:
+    if my_list == []:
         return None
-    length = len(list_)
+    length = len(my_list)
 
     start, end = 0, length - 1
     while start < end:
         mid = start + (end - start) // 2
-        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
-            return list_[mid]
-        if list_[mid - 1] > list_[mid + 1]:
+        if my_list[mid] > my_list[mid - 1] and my_list[mid] > my_list[mid + 1]:
+            return my_list[mid]
+        if my_list[mid - 1] > my_list[mid + 1]:
             end = mid
         else:
             start = mid + 1
-    return list_[start]
+    return my_list[start]
