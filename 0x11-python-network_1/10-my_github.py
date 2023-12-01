f0d99e17a1dbf8b78343a398581087302d1c98b9
Muliro <mulirokhaemba@gmail.com>
2023-12-01 16:46:29 +0300

    pycodestyle

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,10 +4,6 @@ the header of the response.
 """
 from requests import get
 from sys import argv
-
-
 if __name__ == "__main__":
-
-
     r = get('https://api.github.com/user', auth=(argv[1], argv[2]))
     print(r.json().get('id'))
