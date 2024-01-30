250652b089325c79494d16e12f46cb71d9d1ea93
Muliro <mulirokhaemba@gmail.com>
2024-01-30 13:02:08 +0300

    bugs

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // Writes a string to a file
 
-const fls = require('fs')
-const argv = process.argv
-const filePath = argv[2]
-const string = argv[3]
+const fls = require('fs');
+const argv = process.argv;
+const filePath = argv[2];
+const string = argv[3];
 
 fls.writeFile(filePath, string, 'utf8', function (err) {
   if (err) {
-    console.log(err)
+    console.log(err);
   }
-})
+});
