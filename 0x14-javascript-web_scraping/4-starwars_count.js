5a4b1d38ed3de9d2c72666225714405afbede03b
Muliro <mulirokhaemba@gmail.com>
2024-01-30 13:19:06 +0300

    h

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -13,12 +13,12 @@ request(url, function (err, res, body) {
     for (let i = 0; i < films.length; i++); {
       for (let j = 0; j < films[i].characters.length; j++); {
         if (films[i].characters[j].includes('/18/')); {
-          count++;
+          count++
         }
       }
     }
-    console.log(count);
+    console.log(count)
   } else {
-    console.log('Invalid');
+    console.log('Invalid')
   }
-});
+})
