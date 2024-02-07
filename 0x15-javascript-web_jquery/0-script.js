0ad503abc61386818e64ef6ab6195d7e1816eb55
Muliro <mulirokhaemba@gmail.com>
2024-02-07 19:53:04 +0300

    more

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,2 +1,5 @@
-const header = document.querySelector('header');
-header.style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', function () {
+    var header = document.querySelector('header');
+    header.style.color = '#FF0000';
+  });
+  
